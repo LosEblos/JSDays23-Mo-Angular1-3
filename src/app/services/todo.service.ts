9c38469565d1f54d39e46a960a21560f694d93af
LosEblos <128365827+LosEblos@users.noreply.github.com>
2023-03-20 13:46:02 +0100

    2 files modified

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -5,17 +5,28 @@ import { Todo } from '../models/todo';
 export class TodoService {
   private todos: Todo[] = [];
 
-  constructor() {}
+  constructor() {
+    console.log('TodoService.constructor');
+  }
 
-  create(todo: Todo) {}
+  create(todo: Todo) {
+    console.log('TodoService.create', todo);
+  }
 
-  get(todoId: number) {}
+  get(todoId: number) {
+    console.log('TodoService.get', todoId);
+  }
 
   getAll(): Todo[] {
+    console.log('TodoService.getAll', this.todos);
     return this.todos;
   }
 
-  update(todo: Todo): void {}
+  update(todo: Todo): void {
+    console.log('TodoService.update', todo);
+  }
 
-  delete(todoId: number): void {}
+  delete(todoId: number): void {
+    console.log('TodoService.delete', todoId);
+  }
 }
